4ad1843e1ef82be5898b6282c08487d3dbdeb723
Matt Rutkowski <mrutkows@us.ibm.com>
2021-12-06 17:13:20 -0600

    Update validate.go

--- cmd/validate.go
+++ cmd/validate.go
@@ -4,15 +4,6 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
-	"github.com/xeipuuv/gojsonschema"
-)
-
-const (
-	SCHEMA_SPDX_2_2_LOCAL = "file://schema/spdx/2.2/spdx-schema.json"
-)
-
-var (
-	supportedSchemas = []string{SCHEMA_SPDX_2_2_LOCAL}
 )
 
 func init() {
@@ -28,30 +19,7 @@ var validateCmd = &cobra.Command{
 	},
 }
 
-func isSchemaSupported(schema string) bool {
-	for _, s := range supportedSchemas {
-
-		if schema == s {
-			return true
-		}
-	}
-	return false
-}
-
 func Validate(schema string, document string) (bool, error) {
-
-	if !isSchemaSupported(schema) {
-		return false, fmt.Errorf("schema [%s] not supported", schema)
-	}
-
-	loader := gojsonschema.NewReferenceLoader(SCHEMA_SPDX_2_2_LOCAL)
-
-	// create a reusable schema object (to validate multiple documents)
-	_, err := gojsonschema.NewSchema(loader)
-
-	if err != nil {
-		return false, err
-	}
 
 	return true, nil
 }
